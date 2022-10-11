fb8e86f6a877f04624e714b74e9258d798f48aa6
Kolebe Shanks <skolebe@yahoo.com>
2022-10-10 21:31:30 -0500

    update classwork

--- server/app.js
+++ server/app.js
@@ -1,19 +1,46 @@
 // 'Import' the Express module instead of http
 const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+const pizzas = require("./routers/pizzas");
 
 dotenv.config();
 
 // Initialize the Express application
 const app = express();
 
+mongoose.connect(process.env.MONGODB)
+const db = mongoose.connection
+
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once(
+    "open",
+    console.log.bind(console, "Successfully opened connection to Mongo!")
+);
+// CORS Middleware
+const cors = (req, res, next) => {
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With,content-type, Accept,Authorization,Origin"
+    );
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+};
+
 const logging = (request, response, next) => {
     console.log(`${request.method} ${request.url} ${Date.now()}`);
     next();
 };
 
+app.use(cors);
 app.use(express.json());
 app.use(logging);
+
 
 // Handle the request with HTTP GET method from http://localhost:4040/status
 app.get("/status", (request, response) => {
@@ -27,6 +54,9 @@ app.get("/echo/:input", (request, response) => {
     const message = request.params.input;
     response.status(418).json({ echo: message });
 });
+
+app.use("/pizzas", pizzas);
+
 const PORT = process.env.PORT || 4040; // we use || to provide a default value
 // Tell the Express app to start listening
 // Let the humans know I am running and listening on 4040
